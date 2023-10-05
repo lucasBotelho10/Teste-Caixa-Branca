0fa0f7dbeb74708ddf3879836f601179d7096946
Lucas Botelho <lucashbotelho@hotmail.com>
2023-10-05 17:11:45 -0300

    Adicionando arquivos em ETAPA-2

--- src/login/User.java
+++ src/login/User.java
@@ -6,32 +6,36 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-  public Connection conectarBD() {
-		Connection conn = null;
-		try {
-			Class.forName("com.mysql.Driver.Manager").newInstance();
-			String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-			conn = DriverManager.getConnection(url);
-		}catch (Exception e) {}
-		return conn;}
-		public String nome = "";
-		public boolean result = false;
-		public boolean verificarUsuario(String login, String senha) {
-			String sql = "";
-			Connection conn = conectarBD();
+  /*1*/ public Connection conectarBD() {
+		/*2*/ Connection conn = null;
+		/*3*/ try {
+			/*4*/ Class.forName("com.mysql.Driver.Manager").newInstance();
+			/*4*/ String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+			/*4*/ conn = DriverManager.getConnection(url);
+		} /*5*/
+		/*6*/ catch (Exception e) {} /*7*/
+		/*8*/ return conn;
+		} /*9*/
+		/*10*/ public String nome = "";
+		/*10*/ public boolean result = false;
+		/*11*/ public boolean verificarUsuario(String login, String senha) {
+			/*12*/ String sql = "";
+			/*12*/ Connection conn = conectarBD();
 			//INSTRUÇÃO SQL
-			sql += "select nome from usuarios ";
-			sql += "where login = " + "'" + login + "'";
-			sql += " and senha = " + "'" + senha + "';";
-			try {
-				Statement st = conn.createStatement();
-				ResultSet rs = st.executeQuery(sql);
-				if(rs.next()) {
-					result = true;
-					nome = rs.getString("nome");}
-			}catch (Exception e) {}
-			return result;}
-				}//fim da classe
+			/*12*/ sql += "select nome from usuarios ";
+			/*12*/ sql += "where login = " + "'" + login + "'";
+			/*12*/ sql += " and senha = " + "'" + senha + "';";
+			/*13*/ try {
+				/*14*/ Statement st = conn.createStatement();
+				/*14*/ ResultSet rs = st.executeQuery(sql);
+				/*15*/ if(rs.next()) {
+					/*16*/ result = true;
+					/*16*/ nome = rs.getString("nome");} /*17*/
+			} /*18*/
+			/*19*/ catch (Exception e) {} /*20*/
+			/*21*/ return result;
+		} /*22*/
+	}//fim da classe
 	
 
 
